5ebe62fcf021f338be41d9260d1037403a431942
Joel <joho.hoffmann@gmx.de>
2022-10-14 17:31:35 +0200

    testat nr 1a erste idee

---
+++ src/Testate/Aufgabe1/Aufgabe1_a.java
@@ -0,0 +1,60 @@
+package Testate.Aufgabe1;
+
+import java.util.concurrent.Semaphore;
+
+public class Aufgabe1_a extends Thread{
+    public static Semaphore lock0 = new Semaphore(1,true);
+    public static Semaphore lock1 = new Semaphore(0,true);
+    public static Semaphore gleis = new Semaphore(1, true);
+    public static int ctr = 0;
+    public int id;
+    public int speed;
+
+    public static void main(String[] args) {
+        new Aufgabe1_a(0, 5000).start();
+        new Aufgabe1_a(1, 2000).start();
+    }
+
+    public Aufgabe1_a(int id, int speed){
+        this.id = id;
+        this.speed = speed;
+    }
+
+    public void run(){
+        while(true){
+        try {
+            System.out.println("Lok " + this.id + " fährt los");
+            Thread.sleep(speed * (2/3));
+            System.out.println("Lok " + this.id + " will über gemeinsames gleis");
+            if (ctr == 0) {
+                if (this.id == 0) {
+                    lock0.acquire();
+                    System.out.println("Lok " + this.id + " fährt über gemeinsames gleis");
+                    Thread.sleep(speed * (1/3));
+                    System.out.println("Lok " + this.id + " verlässt das gemeinsames gleis");
+                    lock1.release();
+                    lock0.release();
+                    ctr++;
+                }
+                if (this.id == 1) {
+                    lock1.acquire();
+                    System.out.println("Lok " + this.id + " fährt über gemeinsames gleis");
+                    Thread.sleep(speed * (1/3));
+                    System.out.println("Lok " + this.id + " verlässt das gemeinsames gleis");
+                    lock1.release();
+                    ctr++;
+                }
+            } else {
+                gleis.acquire();
+                System.out.println("Lok " + this.id + " fährt über gemeinsames gleis");
+                Thread.sleep(1000);
+                System.out.println("Lok " + this.id + " verlässt das gemeinsames gleis");
+                ctr++;
+                gleis.release();
+            }
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        }
+    }
+}
